74d327676698be1b94920c3235ae7ecad817e64c
ayushdh96 <ayushdh96@gmail.com>
2025-08-20 09:55:32 -0400

    Changing the file for the backend deployment, specifying the file version and also changes to the url. Let's see if this deploys the backend

--- diarization-ui/src/App.jsx
+++ diarization-ui/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [backendMessage, setBackendMessage] = useState('');
 
   useEffect(() => {
-    fetch('https://ayushdh96-mie-diarization-proxmox-main.opensource.mieweb.org/api/test')
+    fetch('http://127.0.0.1:5001/api/test')
       .then((res) => res.json())
       .then((data) => {setBackendMessage(data.message)
         console.log('Backend message:', data.message);
