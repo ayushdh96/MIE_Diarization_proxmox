467be933765a85483142fc905e7edd72e560e6d1
ayushdh96 <ayushdh96@gmail.com>
2025-08-20 11:44:04 -0400

    adding proxy code for the vitejs

--- diarization-ui/vite.config.js
+++ diarization-ui/vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
+  server: {
+    port: 5173,
+    proxy: {
+      "/api": "http://localhost:5001",
+    },
+    host: "0.0.0.0",
+    allowedHosts: ["maxklema-mie-diarization-main.opensource.mieweb.org"],
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
