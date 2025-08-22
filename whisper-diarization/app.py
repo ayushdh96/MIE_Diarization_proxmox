29f7c8201e8e3f6048003cdc4360d145ceea531d
ayushdh96 <ayushdh96@gmail.com>
2025-08-22 16:19:46 -0400

    Pushing the changes for the cors and resolving some of the UI issues.

--- whisper-diarization/app.py
+++ whisper-diarization/app.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 OZWELL_API_KEY = os.getenv("OZWELL_API_KEY")
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "https://ayushdh96-mie-diarization-proxmox-main.opensource.mieweb.org"]}})
 
 @app.route('/api/test', methods=['GET'])
 def test():
